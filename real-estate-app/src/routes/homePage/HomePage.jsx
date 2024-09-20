bb7e906af605c926eab78861de5e1513fbee229a
Kiril-Lankov <k1r3t0@gmail.com>
2024-09-20 22:42:10 +0300

    Search bar created

--- real-estate-app/src/routes/homePage/HomePage.jsx
+++ real-estate-app/src/routes/homePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import SearchBar from "../../components/searchbar/SearchBar";
 import "./homePage.scss";
 
 function HomePage() {
@@ -7,8 +8,25 @@ function HomePage() {
             <div className="textContainer">
                 <div className="wrapper">
                     <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        In fuga ratione nostrum modi? Fugit voluptatum eos autem, expedita consequatur ratione?</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        In fuga ratione nostrum modi? Fugit voluptatum eos autem, expedita consequatur ratione?
+                    </p>
+                    <SearchBar/>
+                    <div className="boxes">
+                        <div className="box">
+                            <h1>16+</h1>
+                            <h2>Years of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>200</h1>
+                            <h2>Award Gained</h2>
+                        </div>
+                        <div className="box">
+                            <h1>1200+</h1>
+                            <h2>Property Ready</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="imgContainer">
