8d2b3237064c29778e15536268f5f66b65374e2d
Kiril-Lankov <k1r3t0@gmail.com>
2024-09-20 21:45:42 +0300

    Homepage created

--- real-estate-app/src/routes/homePage/HomePage.jsx
+++ real-estate-app/src/routes/homePage/HomePage.jsx
@@ -4,7 +4,13 @@ function HomePage() {
 
     return (
         <div className="homePage">
-            <div className="textContainer">text</div>
+            <div className="textContainer">
+                <div className="wrapper">
+                    <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        In fuga ratione nostrum modi? Fugit voluptatum eos autem, expedita consequatur ratione?</p>
+                </div>
+            </div>
             <div className="imgContainer">
                 <img src="/bg.png" alt="" />
             </div>
