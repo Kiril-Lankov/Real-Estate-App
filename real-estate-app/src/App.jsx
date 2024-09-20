db36bfcb37511017798f679d67dd8447ea480e3a
Kiril-Lankov <k1r3t0@gmail.com>
2024-09-20 21:41:14 +0300

    Navbar created

---
+++ real-estate-app/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Navbar from './components/navbar/Navbar';
+import './layout.scss';
+import HomePage from './routes/homePage/HomePage';
+
+function App() {
+ 
+
+  return (
+    <div className='layout'>
+      <div className="navbar">
+       <Navbar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ real-estate-app/src/routes/homePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import "./homePage.scss";
+
+function HomePage() {
+
+    return (
+        <div className="homePage">
+            <div className="textContainer">text</div>
+            <div className="imgContainer">
+                <img src="/bg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
