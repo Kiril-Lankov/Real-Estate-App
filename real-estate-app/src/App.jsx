db474c592a3490de4abebc6be22d86e510174a9c
Kiril-Lankov <k1r3t0@gmail.com>
2024-09-21 21:48:43 +0300

    Layout element created and used via react router

--- real-estate-app/src/App.jsx
+++ real-estate-app/src/App.jsx
@@ -1,21 +1,35 @@
-
-import './App.css'
-import Navbar from './components/navbar/Navbar';
-import './layout.scss';
 import HomePage from './routes/homePage/HomePage';
+import {createBrowserRouter,
+  RouterProvider,
+  
+} from "react-router-dom";
+import Layout from './routes/layout/Layout';
+import ListPage from './routes/listPage/ListPage';
+
 
 function App() {
- 
+
+  const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <HomePage/>
+        },
+        {
+          path: "/list",
+          element: <ListPage/>
+        }
+      ]
+    
+  }
+  ]);
 
   return (
-    <div className='layout'>
-      <div className="navbar">
-       <Navbar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-    </div>
+    
+   <RouterProvider router={router}/>
   );
 }
 
---
+++ real-estate-app/src/routes/listPage/ListPage.jsx
@@ -0,0 +1,10 @@
+import "./listPage.scss";
+
+function ListPage() {
+    
+    return (
+        <div className="listPage">ListPage</div>
+    )
+}
+
+export default ListPage;
