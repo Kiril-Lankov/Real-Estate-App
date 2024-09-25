24d0a359ace7f764a50024bc3b86fe1cea4ca6d7
Kiril-Lankov <k1r3t0@gmail.com>
2024-09-25 14:57:05 +0300

    Chat component created and logic about its functionality - hide and show added

--- real-estate-app/src/components/chat/Chat.jsx
+++ real-estate-app/src/components/chat/Chat.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./chat.scss";
 
 function Chat() {
+    //functionality about chat - show Chatbox if we have any chat messages and hide if we do not
+
+    const [chat, setChat] = useState(true);
     return (
         <div className="chat">
             <div className="messages">
@@ -34,7 +38,45 @@ function Chat() {
                     </p>
                 </div>
             </div>
-            <div className="chatBox">Box</div>
+            { chat && (<div className="chatBox">
+                <div className="top">
+                    <div className="user">
+                        <img src="/john-doe.jpg" alt="" />
+                        John Doe
+                    </div>
+                    <span className="close" onClick={()=>{setChat(null)}}>X</span> 
+                </div>
+                <div className="center">
+                    <div className="chatMessage">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage own">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage own">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage own">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
+                        <span>1 hour ago</span>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <textarea></textarea>
+                    <button>Send</button>
+                </div>
+            </div>)}
         </div>
     )
 }
