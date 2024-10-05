458d16d01fa8d0292f127d14ce29eae7843a19ed
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-05 16:05:31 +0300

    added update function to profile update page

--- Backend/api/controllers/user.controller.js
+++ Backend/api/controllers/user.controller.js
@@ -48,7 +48,9 @@ export const updateUser = async (req, res) => {
             }
         });
 
-        res.status(200).json(updatedUser);
+        const {password:userPassword, ...rest} = updatedUser;
+
+        res.status(200).json(rest);
     } catch (error) {
         console.log(error)
         res.status(500).json({message: "Failed to update user!"})
--- Backend/client/src/routes/profileUpdatePage/ProfileUpdatePage.jsx
+++ Backend/client/src/routes/profileUpdatePage/ProfileUpdatePage.jsx
@@ -1,15 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./profileUpdatePage.scss";
 import { AuthContext } from "../../context/AuthContext";
-
+import apiRequest from "../../lib/apiRequest"
+import {useNavigate} from "react-router-dom"
 function ProfileUpdatePage() {
-
+    const [error, setError] = useState("")
     const { currentUser, updateUser } = useContext(AuthContext);
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      const formData = new FormData(e.target)
+
+      const {username, email, password} = Object.fromEntries(formData)
+
+      try {
+        const res = await apiRequest.put(`/users/${currentUser.id}`, {
+          username,
+          email,
+          password
+        });
+        updateUser(res.data)
+        navigate("/profile")
+        console.log(res.data)
+      } catch (error) {
+        console.log(error)
+        setError(error.response.data.message)
+      }
+    }
     return (
 <div >
 <div className="profileUpdatePage">
       <div className="formContainer">
-        <form > 
+        <form onSubmit={handleSubmit}> 
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -34,7 +58,7 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
-        {/*  {error && <span>error</span>} */}
+          {error && <span>error</span>} 
        </form> 
       </div>
       <div className="sideContainer">
