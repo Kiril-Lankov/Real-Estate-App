3f026f953c15229534a424cfac6c05a3e0e95acc
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-06 20:37:28 +0300

    list page loader created

--- Backend/client/src/lib/loader.js
+++ Backend/client/src/lib/loader.js
@@ -4,3 +4,9 @@ export const singlePageLoader = async ({request, params}) => {
     const res = await apiRequest("/posts/"+params.id)
     return res.data;
 }
+
+export const listPageLoader = async ({request, params}) => {
+    console.log(request)
+    //const res = await apiRequest("/posts/"+params.id)
+    //return res.data;
+}
