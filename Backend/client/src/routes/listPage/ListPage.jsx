816d3978623bbef04880ed4296ebc4f4315f5adf
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-07 12:04:45 +0300

    filter updated

--- Backend/client/src/routes/listPage/ListPage.jsx
+++ Backend/client/src/routes/listPage/ListPage.jsx
@@ -11,35 +11,36 @@ function ListPage() {
     const data = useLoaderData()
     return (
         <div className="listPage">
-            <div className="listContainer">
-                <div className="wrapper">
-                    <Filter/>
-                    <Suspense fallback={<p>Loading...</p>}>
-                    <Await
-                    resolve={data.postResponse}
-                    errorElement={<p>Error loading posts!</p>}
-                    >
-                        {(postResponse) => postResponse.data.map(post => (
-                            <Card key={post.id} item={post}/>
-                        ))}
-                        </Await>
-                        </Suspense>
-                    
-                </div>
-            </div>
-            <div className="mapContainer">
-            <Suspense fallback={<p>Loading...</p>}>
+          <div className="listContainer">
+            <div className="wrapper">
+              <Filter />
+              <Suspense fallback={<p>Loading...</p>}>
                 <Await
-                    resolve={data.postResponse}
-                    errorElement={<p>Error loading posts!</p>}
-                    >
-                    {(postResponse) =>  <Map items={postResponse.data}/>}
+                  resolve={data.postResponse}
+                  errorElement={<p>Error loading posts!</p>}
+                >
+                  {(postResponse) =>
+                    postResponse.data.map((post) => (
+                      <Card key={post.id} item={post} />
+                    ))
+                  }
                 </Await>
-            </Suspense>
-               
+              </Suspense>
             </div>
+          </div>
+          <div className="mapContainer">
+            <Suspense fallback={<p>Loading...</p>}>
+              <Await
+                resolve={data.postResponse}
+                errorElement={<p>Error loading posts!</p>}
+              >
+                {(postResponse) => <Map items={postResponse.data} />}
+              </Await>
+            </Suspense>
+          </div>
         </div>
-    )
-}
+      );
+    }
+    
 
 export default ListPage;
