4730df6e7129513a2d65516f599d42647c18f8e8
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-07 16:59:45 +0300

    new routes created

--- Backend/api/app.js
+++ Backend/api/app.js
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 const app = express();
 
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
