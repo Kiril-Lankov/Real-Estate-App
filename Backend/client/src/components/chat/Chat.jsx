c8042833c8c80d523c14081feb27a898cb6769f8
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-07 19:21:46 +0300

    Chat.jsx updated

--- Backend/client/src/components/chat/Chat.jsx
+++ Backend/client/src/components/chat/Chat.jsx
@@ -1,81 +1,86 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./chat.scss";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
+import { format } from "timeago.js";
 
 function Chat({chats}) {
     //functionality about chat - show Chatbox if we have any chat messages and hide if we do not
 
-    const [chat, setChat] = useState(true);
+    const [chat, setChat] = useState(null);
+    const {currentUser} = useContext(AuthContext);
+
+    const handleOpenChat = async (id, receiver) => {
+        try {
+            const res =     await apiRequest("/chats/" + id);
+            setChat({...res.data, receiver})
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const text = formData.get("text");
+
+        if (!text) return;
+
+        try {
+            const res = await apiRequest.post("/messages/" + chat.id, {text} );
+            setChat(prev=>({...prev, messages:[...prev.messages, res.data]}));
+            e.target.reset();
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="chat">
             <div className="messages">
                 <h1>Message</h1>
-                <div className="message">
-                    <img src="/john-doe.jpg" alt="" />
-                    <span>John Doe</span>
+                {chats?.map((c)=> (
+                    <div className="message" key={c.id}
+                    style={{
+                        backgroundColor: c.seenBy.includes(currentUser.id)
+                        ? "white"
+                        : "#fecd514e"
+                    }}
+                    onClick={()=> handleOpenChat(c.id, c.receiver)}>
+                    <img src={c.receiver.avatar || "/noavatar.jpg"} alt="" />
+                    <span>{c.receiver.username}</span>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, hic.
+                        {c.lastMessage}
                     </p>
                 </div>
-                <div className="message">
-                    <img src="/john-doe.jpg" alt="" />
-                    <span>John Doe</span>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, hic.
-                    </p>
-                </div>
-                <div className="message">
-                    <img src="/john-doe.jpg" alt="" />
-                    <span>John Doe</span>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, hic.
-                    </p>
-                </div>
-                <div className="message">
-                    <img src="/john-doe.jpg" alt="" />
-                    <span>John Doe</span>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, hic.
-                    </p>
-                </div>
+            ))}
+                
             </div>
             { chat && (<div className="chatBox">
                 <div className="top">
                     <div className="user">
-                        <img src="/john-doe.jpg" alt="" />
-                        John Doe
+                        <img src={chat.receiver.avatar || "noavatar.jpg"} alt="" />
+                        {chat.receiver.username}
                     </div>
                     <span className="close" onClick={()=>{setChat(null)}}>X</span> 
                 </div>
                 <div className="center">
-                    <div className="chatMessage">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
-                        <span>1 hour ago</span>
+                    {chat.messages.map((message)=>(
+                   <div className="chatMessage" 
+                   style={{
+                    alignSelf: message.userId === currentUser.id ? "flex-end" : "flex-start",
+                    textAlign: message.userId === currentUser.id ? "right" : "left"
+                   }} 
+                   key={message.id}>
+                        <p>{message.text}</p>
+                        <span>{format(message.createdAt)}</span>
                     </div>
-                    <div className="chatMessage own">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
-                        <span>1 hour ago</span>
-                    </div>
-                    <div className="chatMessage">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
-                        <span>1 hour ago</span>
-                    </div>
-                    <div className="chatMessage own">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
-                        <span>1 hour ago</span>
-                    </div>
-                    <div className="chatMessage">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
-                        <span>1 hour ago</span>
-                    </div>
-                    <div className="chatMessage own">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sapiente!</p>
-                        <span>1 hour ago</span>
-                    </div>
+                    ))}
                 </div>
-                <div className="bottom">
-                    <textarea></textarea>
+                <form onSubmit={handleSubmit} className="bottom">
+                    <textarea name="text"></textarea>
                     <button>Send</button>
-                </div>
+                </form>
             </div>)}
         </div>
     )
