0c364a2dafafe53a7484c8c75300af37e5d80112
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-07 22:09:25 +0300

    Notifications added

--- Backend/api/routes/user.route.js
+++ Backend/api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser,savePost , profilePosts} from "../controllers/user.controller.js";
+import { deleteUser, getUser, getUsers, updateUser,savePost , profilePosts, getNotificationNumber} from "../controllers/user.controller.js";
 
 import {verifyToken} from "../middleware/verifyToken.js"
 const router = express.Router();
@@ -15,6 +15,8 @@ router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
 
 router.get("/profilePosts", verifyToken, profilePosts);
+
+router.get("/notification", verifyToken, getNotificationNumber)
 
 
 export default router;
--- Backend/client/src/components/chat/Chat.jsx
+++ Backend/client/src/components/chat/Chat.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../lib/apiRequest";
 import { format } from "timeago.js";
 import { SocketContext } from "../../context/SocketContext";
+import { useNotificationStore } from "../../lib/notificationStore";
 
 function Chat({chats}) {
     //functionality about chat - show Chatbox if we have any chat messages and hide if we do not
@@ -14,6 +15,8 @@ function Chat({chats}) {
 
     const messageEndRef = useRef();
 
+    const decrease = useNotificationStore((state)=>state.decrease);
+
     useEffect(()=> {
         messageEndRef.current?.scrollIntoView({behaviour: "smooth"});
     },[chat])
@@ -21,6 +24,9 @@ function Chat({chats}) {
     const handleOpenChat = async (id, receiver) => {
         try {
             const res =     await apiRequest("/chats/" + id);
+            if (!res.data.seenBy.includes(currentUser.id)) {
+                decrease();
+            }
             setChat({...res.data, receiver})
         } catch (error) {
             console.log(error)
